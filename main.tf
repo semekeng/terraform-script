e2c779453bb1bf328331a91588c6a9c660aa29d4
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-21 19:51:06 +0530

    Create default-route-table, configure created myapp-igw Create default-route-table & configure the created internet gateway to allow connections to/from internet. However subnet is not associated with this main/default route table because, the default-route-table auto takes all the subnets which does not explicitly associate to any route table

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
 }
 
-resource "aws_route_table" "myapp-route-table" {
+/*resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   route {
@@ -50,9 +50,21 @@ resource "aws_route_table" "myapp-route-table" {
   tags = {
     Name: "${var.env_prefix}-rtb"
   }
-}
+}*/
 
-resource "aws_route_table_association" "a-rtb-subnet" {
+/*resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}*/
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
 }
