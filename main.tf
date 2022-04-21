c8ba21609b00cf2c84e7c0338b78b8202d01e049
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-21 18:34:12 +0530

    Clean terraform script for ec2-demo project

--- main.tf
+++ main.tf
@@ -11,50 +11,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "v_cidr_blocks" {
-  description = "cidr block & name tags for vpc & subnets"
-  # default = "10.0.30.0/24"
-  type = list(object({
-    cidr_block = string
-    name = string
-  }))
-}
-variable "availability_zone" {}
+variable v_vpc_cidr_block {}
+variable v_subnet_cidr_block {}
+variable availability_zone {}
+variable env_prefix {}
 
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.v_cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.v_vpc_cidr_block
   tags = {
-    Name: var.v_cidr_blocks[0].name
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
-  vpc_id     = aws_vpc.development-vpc.id
-  cidr_block = var.v_cidr_blocks[1].cidr_block
+  vpc_id     = aws_vpc.myapp-vpc.id
+  cidr_block = var.v_subnet_cidr_block
   availability_zone = var.availability_zone
   tags = {
-    Name = var.v_cidr_blocks[1].name
+    Name = "${var.env_prefix}-subnet-1"
   }
-}
-
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-  cidr_block = var.v_cidr_blocks[2].cidr_block
-  availability_zone = var.availability_zone
-  tags = {
-    Name = var.v_cidr_blocks[2].name
-  }
-}
-
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
-}
-
-output "aws_subnet-id" {
-  value = aws_subnet.dev-subnet-2.id
 }
 
